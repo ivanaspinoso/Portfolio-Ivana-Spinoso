d8b73d285155d4e3c69fd5f01f7a7c5474a093b2
Ivana Spinoso <138499741+ivanaspinoso@users.noreply.github.com>
2024-05-21 15:11:00 -0300

    Delete HenryDogs directory

--- HenryDogs/PI-Dogs/client/src/redux/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunkMiddleware from 'redux-thunk'
-import reducer from '../reducer/reducer';
-
-export const store = createStore(
-    reducer,
-    composeWithDevTools(applyMiddleware(thunkMiddleware))
-);
